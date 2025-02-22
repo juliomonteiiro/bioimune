bba483c0e308601fa714b6d2680a5e568a6273f7
juliomonteiiro <julioalexsandro.monteiro13@gmail.com>
2025-02-22 03:15:43 -0300

    fix: correction GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/bioimune',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
